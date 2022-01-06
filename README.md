cd5c5dfc382303f530ed71cd791b7c922f1e71f8
botpage <nagrue489@gmail.com>
2022-01-06 13:44:02 -0500

    update package

---
+++ README.md
@@ -0,0 +1,8 @@
+# drkbot-npm
+A NPM Package for DrkBot
+
+### NPM Installion
+`npm install -g drkbot-npm`
+
+### NPM Author
+`ian` - [NPM](https://www.npmjs.com/package/drkbot-npm)
---
+++ src/drk.js
@@ -0,0 +1,12 @@
+exports.clear = require("../lib/data/clear_msg");
+exports.error = require("../lib/data/error_msg");
+exports.startwhatsasena = require("../lib/data/start");
+exports.update = require("../lib/data/update");
+exports.work_type = require("../lib/data/worktype");
+
+/*
+exports.daily_announcement = require("../lib/data/announcement");
+exports.timezone = require("../lib/data/timezone_setter");
+exports.get_eva_ai = require("../lib/data/ctn");
+exports.ephoto = require("../lib/data/new_ephoto");
+*/
